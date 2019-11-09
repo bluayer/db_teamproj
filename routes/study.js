5dac135a0bb67db8dfa3458faee1a006ce2f1704
SongJungwoo <ijacsong98@gmail.com>
2019-11-09 22:12:49 +0900

    Front name changed and add show pages
    Footprint is changed as easypath.
    Also, footprint and study has show pages that literally shows the data of single row(maybe).

--- routes/study.js
+++ routes/study.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const Console = console;
 
 router.get('/', (req, res, next) => {
-  res.render('../views/footprint/index');
+  res.render('../views/study/index');
 });
+
+router.get('/show', (req, res, next) => {
+  res.render('../views/study/show');
+});
+
 router.get('/new', (req, res, next) => {
-  res.render('../views/footprint/new');
+  res.render('../views/study/new');
 });
 
 
