a33c98313899683f29ee041a17b32cac56c60bb2
HSUABI <seongwooksong2017@gmail.com>
2019-11-14 14:22:13 +0900

    Study search, Easypath search

--- routes/easypath.js
+++ routes/easypath.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('./db');
 const Console = console;
 
 router.get('/', (req, res, next) => {
@@ -18,12 +19,45 @@ router.get('/new', (req, res, next) => {
 });
 
 router.get('/posts/search', (req, res, next) => {
-  console.log("hello")
-  const { searchCategory, searchWord } = req.body;
-  const sql = `SELECT * FROM REALLY_FINAL_DB.TBL_EASYPATH_INFO WHERE user_id = (SELECT user_id FROM REALLY_FINAL_DB.TBL_USER_INFO WHERE username = ?);`;
-  const requirements = [searchWord];
+  var searchWord = req.param('searchWord');
+  var searchCategory = req.param('searchCategory');
 
-  console.log(sql)
+  //consol.log는 printf같아서 디버깅하면댐
+  //console.log(searchCategory);
+
+  //if else문에는 sql 작성을하는데 변수는${} 식으로해서 넣음
+  if(searchCategory=='author')
+  {
+  	var sql = `SELECT * FROM REALLY_FINAL_DB.TBL_EASYPATH_INFO WHERE user_id = (SELECT user_id FROM REALLY_FINAL_DB.TBL_USER_INFO WHERE username = '${searchWord}');`;
+  	console.log(sql);
+  }
+  else
+  {
+  	var sql = `SELECT * FROM REALLY_FINAL_DB.TBL_EASYPATH_INFO
+	WHERE easypath_title LIKE '%${searchWord}%'
+	UNION SELECT *
+	FROM REALLY_FINAL_DB.TBL_EASYPATH_INFO
+	WHERE easypath_content LIKE '%${searchWord}%';`
+	console.log(sql)
+
+  }
+
+  //이부분이 sql보내는부분  정우는 requirement가들어갓는데 난오류떠서 뺴버렷음
+  connection.query(sql,(error, results, fields) => {
+    if (error) {//에러가뜨면 이쪽을실행함
+      Console.log('error is' + error);
+      res.write("<script language=\"javascript\">alert('unknown input')</script>");	//alert는알림띄우는거
+      res.write("<script language=\"javascript\">window.location=\"/easypath\"</script>");
+      res.end();
+    } else { //성공하면 이걸실행함 result에 쿼리결과가들어감
+      console.log('\nStudy_query success');
+      console.log(results);
+      res.redirect('/easypath')
+
+    }
+  })
+
+  
 }) 
 
 
