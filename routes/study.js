fb6b69c0972ce22a86a402aac3c50bc078a173e3
HSUABI <seongwooksong2017@gmail.com>
2019-11-15 22:42:08 +0900

    스터디 만들기 기능 구현

--- routes/study.js
+++ routes/study.js
@@ -18,8 +18,70 @@ router.get('/', (req, res, next) => {
 router.get('/new', (req, res, next) => {
   const { session } = req;
   console.log("등록하기 페이지")
-  res.render('../views/easypath/new', { session });
+  res.render('../views/study/new', { session });
 });
+
+// 스터디 등록하기에서 값 입력하고 등록하기 버튼클릭햇을때 처리
+router.post('/create', (req, res, next) => {
+  const { session } = req;
+  const { user_id } = req.session;
+  var easypath_id = parseInt(req.param("easypath_id")); //지금 front에 easypath관련 등록부분이없어서 1로 상수정함
+
+  var study_title = req.param('title');
+  var study_content = req.param('content');
+  var max_num_people = parseInt(req.param('person_num')); //'1'이렇게 와서 이걸 정수형으로바꿈
+  //var cur_num_people = 1; database에서 default값을 1로해주었음
+  var study_location = req.param('location');
+  //var email = req.param('email'); 이건 데이터베이스 다시수정하면서 없앰
+
+  //<!--데이터넘어 갈때는 1,2,3이렇게 넘어감-->
+  //그래서 1일떄 서울, 2일때 경기 이렇게 다시 명시해줘야댐
+  if (study_location ==1)
+  {
+    study_location='서울';
+  }
+  else if (study_location ==2)
+  {
+    study_location='경기';
+  }
+  else if (study_location ==3)
+  {
+    study_location='인천';
+  }
+
+  /*
+  console.log(user_id);
+  console.log(easypath_id);
+  console.log(study_title);
+  console.log(study_content);
+  console.log(max_num_people);
+  console.log(study_location);
+  */
+
+  var sql = `INSERT INTO REALLY_FINAL_DB.TBL_STUDY_INFO(user_id, easyPath_id, study_title, study_content, max_num_people, study_location, created_time, updated_time) 
+VALUES (${user_id}, ${easypath_id}, "${study_title}", "${study_content}", ${max_num_people}, '${study_location}', 0, 0); `;
+
+  connection.query(sql,(error, results, fields) => {
+    if (error) {//에러가뜨면 이쪽을실행함
+      Console.log('error is' + error);
+      res.write("<script language=\"javascript\">alert('check easypath id again')</script>"); //alert는알림띄우는거
+      res.write("<script language=\"javascript\">window.location=\"/study\"</script>");
+      res.end();
+    } else { //성공하면 이걸실행함 result에 쿼리결과가들어감
+      console.log('\nStudyCreate query success');
+      console.log(sql);
+
+      //내가 redirect를 한이유는 http://localhost:3000/study/로 가기위해서임
+      //그러면  http://localhost:3000/study/ 가 요청이됥태고 
+      //그러면 router.get('/', (req, res, next)가 실행이됨 이 기능이 수행되면  study목록을 다가져옴
+      //그래서 새로추가한 study정보까지 다불러옴
+      res.redirect("./");
+
+    }
+  })
+  
+});
+
 
 // 스터디 가입하기
 router.post('/apply', (req, res, next) => {
