05c93d4832dd996a157152887bd27cffca94fd51
SongJungwoo <ijacsong98@gmail.com>
2019-11-28 14:46:39 +0900

    New study errors are solved
    There's some errors in new study.
    First of all, study_location is weird when user puts input.
    Secondly, why do you get easypath_id as input?
    I can't understand your thought - Jisu Kim
    So it linked with easypath_id automatically, when user click new study button.

--- routes/study.js
+++ routes/study.js
@@ -18,47 +18,29 @@ router.get('/', (req, res, next) => {
 
 
 // 스터디 등록하기
-router.get('/new', (req, res, next) => {
+router.post('/new', (req, res, next) => {
   const { session } = req;
+  const { easypath_id } = req.body
   console.log("등록하기 페이지")
-  res.render('../views/study/new', { session });
+  res.render('../views/study/new', { session, easypath_id });
 });
 // 스터디 등록하기에서 값 입력하고 등록하기 버튼클릭햇을때 처리
 router.post('/create', (req, res, next) => {
   const { session } = req;
   const { user_id } = req.session;
-  var easypath_id = parseInt(req.param("easypath_id")); //지금 front에 easypath관련 등록부분이없어서 1로 상수정함
+  const { easypath_id, study_title, study_content, study_location } = req.body; 
 
-  var study_title = req.param('title');
-  var study_content = req.param('content');
   var max_num_people = parseInt(req.param('person_num')); //'1'이렇게 와서 이걸 정수형으로바꿈
   //var cur_num_people = 1; database에서 default값을 1로해주었음
-  var study_location = req.param('location');
-  //var email = req.param('email'); 이건 데이터베이스 다시수정하면서 없앰
 
-  //<!--데이터넘어 갈때는 1,2,3이렇게 넘어감-->
-  //그래서 1일떄 서울, 2일때 경기 이렇게 다시 명시해줘야댐
-  if (study_location ==1)
-  {
-    study_location='서울';
-  }
-  else if (study_location ==2)
-  {
-    study_location='경기';
-  }
-  else if (study_location ==3)
-  {
-    study_location='인천';
-  }
 
-  /*
   console.log(user_id);
   console.log(easypath_id);
   console.log(study_title);
   console.log(study_content);
   console.log(max_num_people);
   console.log(study_location);
-  */
+
 
   var sql = `INSERT INTO REALLY_FINAL_DB.TBL_STUDY_INFO(user_id, easyPath_id, study_title, study_content, max_num_people, study_location, created_time, updated_time)
 VALUES (${user_id}, ${easypath_id}, "${study_title}", "${study_content}", ${max_num_people}, '${study_location}', 0, 0); `;
